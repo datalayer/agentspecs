b9c9acd87b11ce8bdc1ad1a7a3ac3a0cac59d8ce
Eric Charles <eric@datalayer.io>
2026-09-03 17:10:46 +0200

    docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -50,7 +50,7 @@ module.exports = {
         },
         {
           type: 'doc',
-          docId: 'modularityue /index',
+          docId: 'modularity/index',
           position: 'left',
           label: 'Modularity',
         },
